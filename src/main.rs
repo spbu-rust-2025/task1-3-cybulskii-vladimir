f1dfbe1cf25b2bc881923dd1b04f3d172e52ccc2
Waldemar <superlexaan@gmail.com>
2025-11-02 19:06:27 +0100

    make code required for the task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::io;
+use std::fs;
 
+fn main() {
+	let mut filepath = String::new();
+	io::stdin()
+		.read_line(&mut filepath)
+		.expect("Failed to read line");
+	if let Ok(contents) = fs::read_to_string(filepath) {
+		println!("success");
+	}
+	else {
+		println!("failure");
+	}
 }
