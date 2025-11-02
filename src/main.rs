11639c925c359e5c2c9453c83be8a7b9cc84e780
Waldemar <superlexaan@gmail.com>
2025-11-02 19:08:17 +0100

    fix minor coding error in variable creation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 	io::stdin()
 		.read_line(&mut filepath)
 		.expect("Failed to read line");
-	if let Ok(contents) = fs::read_to_string(filepath) {
+	if let Ok(_contents) = fs::read_to_string(filepath) {
 		println!("success");
 	}
 	else {
